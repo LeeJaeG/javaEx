df79aa7443d06300d2cc9e325d8712efa3c468d2
HP <HP@DIP-PC>
2022-06-23 18:46:34 +0900

    weqqw

---
+++ project/Inheritance/src/Computer.java
@@ -0,0 +1,13 @@
+
+public class Computer extends Calcuator {
+
+
+//@Override
+
+//double areaCircle(double r) {
+//	System.out.println("Computer 媛�泥댁�� areaCircle�� �ㅽ��");
+//	return Math.PI*r*r;
+
+	
+//	}
+}
